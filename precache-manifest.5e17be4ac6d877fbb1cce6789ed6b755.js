f2a8eabe26fb29079648a7923a8d4b292000c285
JuanCarlos <juancarlos318504@gmail.com>
2020-09-28 17:37:01 -0500

    Updates

---
+++ precache-manifest.5e17be4ac6d877fbb1cce6789ed6b755.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10212bac1d3c6b73248721120449a54d",
+    "url": "/tramite-SISBEN/index.html"
+  },
+  {
+    "revision": "47fd8748c6fa62b2a99a",
+    "url": "/tramite-SISBEN/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "01ff67aab8db72bbb001",
+    "url": "/tramite-SISBEN/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47fd8748c6fa62b2a99a",
+    "url": "/tramite-SISBEN/static/js/2.6a0095bb.chunk.js"
+  },
+  {
+    "revision": "5e3acc34ba6a33ba106536de5ceb05f3",
+    "url": "/tramite-SISBEN/static/js/2.6a0095bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ff67aab8db72bbb001",
+    "url": "/tramite-SISBEN/static/js/main.728c5590.chunk.js"
+  },
+  {
+    "revision": "a81295dcc2714c39034c",
+    "url": "/tramite-SISBEN/static/js/runtime-main.37531a72.js"
+  },
+  {
+    "revision": "a287e97f0a92c721c88e8d05bc1679a7",
+    "url": "/tramite-SISBEN/static/media/camara.a287e97f.ico"
+  },
+  {
+    "revision": "4e75923f8b10c9818a3b5ab18bfe4d84",
+    "url": "/tramite-SISBEN/static/media/micro2.4e75923f.webp"
+  },
+  {
+    "revision": "8f52fda7ecdd5b545cff1fabd8a2a037",
+    "url": "/tramite-SISBEN/static/media/microred.8f52fda7.png"
+  }
+]);
